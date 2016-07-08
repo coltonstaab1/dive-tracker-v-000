a5aba3b72b0f9fafa09760ccbe4391ff6e0727b0
coltonstaab1 <colton.staab@gmail.com>
2016-07-08 13:24:55 -0400

    Migrated database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160708171306) do
+ActiveRecord::Schema.define(version: 20160708172319) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -45,6 +45,39 @@ ActiveRecord::Schema.define(version: 20160708171306) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "dive_types", force: :cascade do |t|
+    t.integer  "dive_number"
+    t.string   "dive_letter"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "dives", force: :cascade do |t|
+    t.integer  "dive_type_id"
+    t.float    "score_1"
+    t.float    "score_2"
+    t.float    "score_3"
+    t.integer  "meet_id"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "meets", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "datetime"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
+    t.string   "hometown"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
