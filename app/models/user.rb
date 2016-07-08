5a234022831bd28c2d2622b20edd33d014600bae
coltonstaab1 <colton.staab@gmail.com>
2016-07-08 14:46:19 -0400

    Clarified the user-dive relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
      :omniauthable, :omniauth_providers => [:facebook, :google]
 
   belongs_to :team
+  has_many :dives
 
   def self.from_omniauth(auth)
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
