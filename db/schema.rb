a83c748f53e44ae48f8f7a8615828d6602f5dd6a
coltonstaab1 <colton.staab@gmail.com>
2016-07-08 12:38:23 -0400

    Added provider UID column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160708163639) do
+ActiveRecord::Schema.define(version: 20160708163748) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160708163639) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider_uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
