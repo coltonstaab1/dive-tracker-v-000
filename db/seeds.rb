750f01719d1881da417c0725eb5bb76e68c6b25e
coltonstaab1 <colton.staab@gmail.com>
2016-07-08 13:37:07 -0400

    Created Team and Meet seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+Team.create(name: "Yale Bulldogs", hometown: "New Haven, CT")
+Team.create(name: "Princeton Tiger", hometown: "Princeton, NJ")
+Team.create(name: "Harvard Crimson", hometown: "Cambridge, MA")
+
+Meet.create(description: "Harvard-Yale-Princeton", datetime: DateTime.new(2016,02,03,12,00,00,'-5'))
+Meet.create(description: "Ivy League Championships", datetime: DateTime.new(2016,02,14,12,00,00,'-5'))
+Meet.create(description: "NCAA Championships", datetime: DateTime.new(2016,04,05,17,00,00,'-5'))
